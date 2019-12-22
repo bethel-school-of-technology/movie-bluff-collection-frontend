7d4bfb27ac9dba48044a915bee9d4f3e7a7a2511
Justin Shannon <42521970+papa7wheelie@users.noreply.github.com>
2019-12-22 13:09:04 -0500

    Set up user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,5 @@
-export class UserSignin {
+export class User {
+    id: number;
     firstName: string;
     lastName: string;
     email: string;
--- src/app/user-signin.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserSignin } from './user-signin';
-
-describe('UserSignin', () => {
-  it('should create an instance', () => {
-    expect(new UserSignin()).toBeTruthy();
-  });
-});
